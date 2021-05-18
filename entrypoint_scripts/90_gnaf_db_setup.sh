f935bf3cf4693593728c6a4ca805296486d1e8b4
rv <rahul.vemulapally@intellify.com.au>
2021-05-18 20:00:02 +1000

    fixed issue scripts when handling new data files

--- entrypoint_scripts/90_gnaf_db_setup.sh
+++ entrypoint_scripts/90_gnaf_db_setup.sh
@@ -6,7 +6,7 @@ This script creates the GNAF DB.
 
 STEPS:
 
-1. Download the GNAF GDA2020 zip file from data.gov.au and extract the contents to a location that is accessible to the PostgreSQL server
+1. Download the GNAF GDA2020 zip file from data.gov.au and extract the contents to "data" folder, this folder gets mapped to "/gnaf_data" folder in the container
 2. Map the table names to file names  (Authority Code & Standard files), output from this step is used in STEP 5
 3. Create the schema
 4. Create the tables
@@ -25,7 +25,7 @@ SCHEMA_="$GNAF_SCHEMA"
 SEARCH_PATH_="$GNAF_SCHEMA,public"
 
 
-## 
+##
 #GNAF_DATA_FOLDER="$(ls /gnaf_data | grep -i gda2020)"        # hacky!
 GNAF_DATA_FOLDER="$(find /gnaf_data -type d -name 'G-NAF' | xargs dirname | xargs basename)"     # looking for G-NAF folder in parent dir
 GNAF_MAIN_DATA_FILES_FOLDER="$(ls /gnaf_data/$GNAF_DATA_FOLDER/G-NAF | grep -Ee "G-NAF\s+[A-Z]+\s+20[0-9]{2}")"
@@ -40,11 +40,11 @@ perl -i -pe "s|file_path_prefix TEXT := .*$|file_path_prefix TEXT := '$DATA_FILE
 
 ## Authority Code tables
 ## Extract Authority Code table names from the psv file names
-ls "$DATA_FILES_PATH/Authority Code/" | awk  'BEGIN{OFS=","} v_ = match($1, /(Authority_Code_)(.*)(_psv.psv)/, arr){print $1,arr[2]}' > "$DATA_FILES_PATH/ac_tables.csv"
+ls "$DATA_FILES_PATH/Authority Code/" | awk  'BEGIN{OFS=","} v_ = match($1, /(Authority_Code_)(.*)(_psv.psv)/, arr){print $1,arr[2]}' > "$DATA_FOLDER_IN_CONTAINER/ac_tables.csv"
 
 ## Standard tables
 ## Extract Standard table names from the psv file names
-ls "$DATA_FILES_PATH/Standard/" | awk  'BEGIN{OFS=","} v_ = match($1, /([A-Z]+)_(.*)(_psv.psv)/, arr){print $1,arr[2]}' > "$DATA_FILES_PATH/std_tables.csv"
+ls "$DATA_FILES_PATH/Standard/" | awk  'BEGIN{OFS=","} v_ = match($1, /([A-Z]+)_(.*)(_psv.psv)/, arr){print $1,arr[2]}' > "$DATA_FOLDER_IN_CONTAINER/std_tables.csv"
 
 
 ## STEP 3
--- scripts/load_data.sql
+++ scripts/load_data.sql
@@ -7,31 +7,33 @@ DECLARE
 
     file_path_prefix TEXT := '/gnaf_data/g-naf_may22_allstates_gda2020_psv_106/G-NAF/G-NAF MAY 2022/';
 
+    data_folder_in_container TEXT := '/gnaf_data/';
+
     -- Path where authority code CSV files are
-    ac_file_path TEXT := file_path_prefix || 'Authority Code/';   
-    
+    ac_file_path TEXT := file_path_prefix || 'Authority Code/';
+
     -- Path where standard CSV files are
-    std_file_path TEXT := file_path_prefix || 'Standard/';   
-    
+    std_file_path TEXT := file_path_prefix || 'Standard/';
+
     fn TEXT;              -- Variable to hold name of current CSV file being inserted
     tn TEXT;              -- Variable to hold table name matching the CSV file
 
-BEGIN  
- 
+BEGIN
+
     -- AUTHORITY CODE TABLE
 
     CREATE TABLE ac_files (
         fileName TEXT,
         tableName TEXT
     );
-    
+
     --ac_tables_file :=  ac_file_path || 'ac_tables.csv';
-    --COPY ac_files FROM ac_tables_file CSV DELIMITER ','; 
-    EXECUTE 'COPY ac_files FROM ''' || file_path_prefix || 'ac_tables.csv'' CSV DELIMITER '',''';
+    --COPY ac_files FROM ac_tables_file CSV DELIMITER ',';
+    EXECUTE 'COPY ac_files FROM ''' || data_folder_in_container || 'ac_tables.csv'' CSV DELIMITER '',''';
     ALTER TABLE ac_files add column processed int default 0;
 
 
-    LOOP    
+    LOOP
         select fileName, tableName INTO fn, tn from ac_files where processed = 0 limit 1; -- Pick the first file
         raise notice 'fn: %', fn;
         EXECUTE 'COPY ' || tn || ' from ''' || ac_file_path ||  fn || ''' with DELIMITER ''|'' csv header';
@@ -48,10 +50,10 @@ BEGIN
     );
 
     --std_tables_file := std_file_path || 'std_tables.csv';
-    EXECUTE 'COPY std_files FROM ''' || file_path_prefix || 'std_tables.csv'' CSV DELIMITER '',''';
+    EXECUTE 'COPY std_files FROM ''' || data_folder_in_container || 'std_tables.csv'' CSV DELIMITER '',''';
     ALTER TABLE std_files add column processed int default 0;
 
-    LOOP    
+    LOOP
         select fileName, tableName INTO fn, tn from std_files where processed = 0 limit 1; -- Pick the first file
         raise notice 'fn: %', fn;
         EXECUTE 'COPY ' || tn || ' from ''' || std_file_path ||  fn || ''' with DELIMITER ''|'' csv header';
